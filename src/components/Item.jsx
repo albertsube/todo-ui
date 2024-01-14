71a9f86a3937217f146495a26c538f7152910167
albertsube <albert.sune.belmonte@gmail.com>
2024-01-14 11:48:57 +0100

    Item styles

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -28,14 +28,14 @@ export default function Item({
     }
 
     return (
-        <div className="grid grid-cols-3 bg-slate-100 p-6 rounded-md shadow-md relative group">
+        <div className="grid grid-cols-3 gap-5 bg-slate-100 p-6 rounded-md shadow-md relative group">
             <div>
                 <h3 className="font-bold text-lg">{title}</h3>
                 <p>{new Date(datestart).toLocaleDateString()}</p>
                 <p>{new Date(dateend).toLocaleDateString()}</p>
                 <p>{user}</p>
             </div>
-            <div className="flex flex-col justify-between col-span-2">
+            <div className="flex flex-col justify-between col-span-2 w-96">
                 <p className="text-left">{description}</p>
                 <p className={`${statusColor[status]} p-2 rounded-md w-fit`}>{status}</p>
             </div>
